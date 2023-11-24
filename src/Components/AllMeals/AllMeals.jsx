7fb9fed6c6fa6857e014489793591b7b019949cd
ShahreyarRafi <shahreyar.rafi@gmail.com>
2023-11-24 23:55:50 +0600

    fixed search not working issue

--- src/Components/AllMeals/AllMeals.jsx
+++ src/Components/AllMeals/AllMeals.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
